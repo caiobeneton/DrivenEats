d393c4917e5a1fd605e5540ad829f99fe8593a04
Caio Beneton <caio.beneton@gmail.com>
2022-08-12 17:18:04 -0300

    Implementação da seleção e dos sinais de checagem

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-function seleciona(item) {
+function seleciona(item, item2) {
 
     const Botselecionado = document.querySelector('.pratos .borda')
     if (Botselecionado !== null) {
         Botselecionado.classList.remove('borda')
     }
+    const check = document.querySelector('.pratos .mostra')
+    if (check !== null) {
+        check.classList.remove('mostra')
+    }
+    
+    const cardcheck = document.querySelector(item2)
+    cardcheck.classList.add('mostra')
 
     const card = document.querySelector(item)
     card.classList.add('borda')
 }
-function selecionaB(item) {
+function selecionaB(item, item2) {
 
     const Botselecionado = document.querySelector('.bebidas .borda')
     if (Botselecionado !== null) {
         Botselecionado.classList.remove('borda')
     }
 
+    const check = document.querySelector('.bebidas .mostra')
+    if (check !== null) {
+        check.classList.remove('mostra')
+    }
+    
+    const cardcheck = document.querySelector(item2)
+    cardcheck.classList.add('mostra')
+
     const card = document.querySelector(item)
     card.classList.add('borda')
 }
-function selecionaS(item) {
+function selecionaS(item, item2) {
 
     const Botselecionado = document.querySelector('.sobremesas .borda')
     if (Botselecionado !== null) {
         Botselecionado.classList.remove('borda')
     }
 
+    const check = document.querySelector('.sobremesas .mostra')
+    if (check !== null) {
+        check.classList.remove('mostra')
+    }
+    
+    const cardcheck = document.querySelector(item2)
+    cardcheck.classList.add('mostra')
+
     const card = document.querySelector(item)
     card.classList.add('borda')
 }
+
